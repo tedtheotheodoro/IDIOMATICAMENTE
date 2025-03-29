01185d1392e38e486137fc17585c788c4bec8214
tedtheotheodoro <tedtheotheodoro@gmail.com>
2025-03-29 16:34:58 -0300

    Remove título duplicado de index.html e move para o componente

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,7 @@ import './index.css'; // Importa o Tailwind!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div className="text-center">
-      <h1 id="title">IDIOMATICAMENTE</h1>
-      <p id="subtitle">Porque algumas expressões não cabem em duas línguas — e tudo bem.</p>
-    </div>
-    {/* Componente idiomaticamente */}
+  {/* Componente idiomaticamente */}
     <IDIOMATICAMENTE />
-  </React.StrictMode>,
+    </React.StrictMode>,
 )
