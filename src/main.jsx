16ded26fe69a77efa86524b58d59c34ce3227724
tedtheotheodoro <tedtheotheodoro@gmail.com>
2025-03-29 15:13:14 -0300

    Centraliza título e ajusta fonte no index e main

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import idiomaticamente from './idiomaticamente'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import idiomaticamente from './idiomaticamente'; // Se necessário, o import pode ser ajustado
 import './index.css'; // Importa o Tailwind!
-import IDIOMATICAMENTE from './idiomaticamente';
-
+import IDIOMATICAMENTE from './idiomaticamente'; // Se necessário, o import pode ser ajustado
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* Título centralizado ao topo */}
+    <h1 id="title">IDIOMATICAMENTE</h1>
+    {/* Componente idiomaticamente */}
     <IDIOMATICAMENTE />
   </React.StrictMode>,
-)
+);
