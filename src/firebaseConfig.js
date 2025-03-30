cc1314a2814c99dbce310f60782ea5a734533598
tedtheotheodoro <tedtheotheodoro@gmail.com>
2025-03-30 13:31:03 -0300

    Atualizar firebase.Config e package-lock.json

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,30 +1,18 @@
-// src/firebase.js
+// src/firebaseConfig.js
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Configuração do Firebase (🔥 ok compartilhar publicamente, essas chaves não dão acesso root)
 const firebaseConfig = {
   apiKey: "AIzaSyB8_uyinOLVz6OQ9nWaT_MMf1BdB7xof_A",
   authDomain: "untranslatable-66db6.firebaseapp.com",
   projectId: "untranslatable-66db6",
-  storageBucket: "untranslatable-66db6.firebasestorage.app",
+  storageBucket: "untranslatable-66db6.appspot.com",
   messagingSenderId: "906137552323",
   appId: "1:906137552323:web:b533f5b4cb6a6312e96976"
 };
 
-// Inicialize o Firebase
-firebase.initializeApp(firebaseConfig);
-
-// Inicializa o app
+// Inicialização
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-// Inicializa Auth e Firestore
-const auth = getAuth(app);
-const firestore = getFirestore(app);
-
-// Inicializa o provedor de login do Google
-const provider = new GoogleAuthProvider();
-
-// Exporta as instâncias
-export { auth, firestore, provider };
+export { db }; // Exportação única (adicione outras se necessário)
